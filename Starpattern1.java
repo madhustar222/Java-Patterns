4026689375c752f1852ae7dc4c614b1a1deab8dc
Kavide Madhu <kavidemadhu759@gmail.com>
2026-04-08 19:10:14 +0530

    added new file

--- Starpattern1.java
+++ Starpattern1.java
@@ -2,7 +2,7 @@ public class Starpattern1 {
     public static void main(String[] madhu){
         for(int i=0;i<5;i++){
             for (int j=0;j<5;j++){
-                System.out.print("X"+" ");
+                System.out.print("*"+" ");
             }
             System.out.println();
         }
