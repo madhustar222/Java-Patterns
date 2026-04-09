2bdff5595bff33a1fd66f5497b35776821f06110
Kavide Madhu <kavidemadhu759@gmail.com>
2026-04-09 22:07:10 +0530

    added Alphabet patterns

---
+++ Alphabetpatt1.java
@@ -0,0 +1,10 @@
+public class Alphabetpatt1{
+    public static void main(String[] madhu){
+        for(int i='A';i<='E';i++){
+            for(int j='A';j<=i;j++){
+                System.out.print((char)j);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Alphabetpatt2.java
@@ -0,0 +1,10 @@
+public class Alphabetpatt2 {
+    public static void main(String[] madhu){
+        for(int i='E';i>='A';i--){
+             for (int j='A';j<=i;j++){
+                System.out.print((char)j);
+             }
+             System.out.println();
+        }
+    }
+}
